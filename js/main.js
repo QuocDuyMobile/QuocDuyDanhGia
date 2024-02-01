99e999d97964b3cd435af8134997b9284aaa31f1
Quốc Duy Mobile <157278606+QuocDuyMobile@users.noreply.github.com>
2024-02-01 20:35:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -756,7 +756,7 @@ async function post() {
 					"Access-Control-Allow-Origin": "*",
       					"Access-Control-Allow-Credentials": "true",
       					"Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      					"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+      					"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, access-token",
 					"Content-Type": 'application/json',
 					"Accept": 'application/json',
 					"access-token": 'sznsb6277f2ff89c4cc6878a03bcb769e72a'
