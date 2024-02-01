2031744b2bc1705ce89bf137c983883ce67e90cc
Quốc Duy Mobile <157278606+QuocDuyMobile@users.noreply.github.com>
2024-02-01 23:52:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -737,6 +737,16 @@ async function post() {
 }
 
 function sendZNS(){
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	function generateString(length) {
+		let result = ' ';
+		const charactersLength = characters.length;
+		for (let i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+
+		return result;
+	}
 	var json = {
 		name: $('#name').val(),
 		phone: $('#phone').val(),
